dfc929d00aa19c66e301112ae73dba5309b8a4cc
Troy Dorman <troydorman@gmail.com>
2021-11-12 20:43:37 -0700

    created new get route to filter database based on parameter sent

--- server/routes/sounds.js
+++ server/routes/sounds.js
@@ -14,13 +14,21 @@ router.get("/sounds", (req, res) => {
     .then((sounds) => res.json(sounds));
 });
 
+// @ROUTE get api/sounds:parameter
+// @desc return various items from the sounds database depending on parameter
+// @access Public
+router.get("/sounds/:parameter", (req, res) => {
+  console.log(req.params.parameter);
+  Sounds.find({ soundType: req.params.parameter })
+    .sort({ date: -1 })
+    .then((sounds) => res.json(sounds));
+});
+
 // @ route GE api/sound:id
 // @ desc  Get sound by id
 // @ access Private
 
 router.get("/sound/:filename", (req, res) => {
-  
-
   res.sendFile(path.join(__dirname, "../public/sounds/", req.params.filename));
 });
 
