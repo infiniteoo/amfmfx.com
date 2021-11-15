afe8977852efdff99e293afa6612080835b85719
Troy Dorman <troydorman@gmail.com>
2021-11-15 13:14:41 -0700

    determining length of chosen audio file and adding to file state

--- src/components/addsound.js
+++ src/components/addsound.js
@@ -19,6 +19,10 @@ const addsound = () => {
     console.log(chosenFiles.files);
     console.log(fileToUpload);
   };
+
+  // Create a non-dom allocated Audio element
+  var audio = document.createElement("audio");
+
   return (
     <form className="add-sound-form">
       <div class="form-group">
@@ -148,7 +152,50 @@ const addsound = () => {
           className="form-control-file"
           id="files"
           name="files"
-          multiple
+          /* multiple */
+          onChange={(e) => {
+            var target = e.currentTarget;
+            var file = target.files[0];
+            var reader = new FileReader();
+
+            if (target.files && file) {
+              var reader = new FileReader();
+
+              reader.onload = function (e) {
+                audio.src = e.target.result;
+                audio.addEventListener(
+                  "loadedmetadata",
+                  function () {
+                    // Obtain the duration in seconds of the audio file (with milliseconds as well, a float value)
+                    var duration = audio.duration;
+
+                    // example 12.3234 seconds
+                    console.log(
+                      "The duration of the song is of: " + duration + " seconds"
+                    );
+                    let time = parseInt(duration);
+                    let minutes = Math.floor(time / 60);
+                    let seconds = time - minutes * 60;
+                    console.log(
+                      `time after calculation: ${minutes}:${seconds}`
+                    );
+
+                    setFileToUpload({
+                      ...fileToUpload,
+                      length: parseInt(duration),
+                    });
+                    // Alternatively, just display the integer value with
+                    // parseInt(duration)
+                    // 12 seconds
+                  },
+                  false
+                );
+              };
+
+              reader.readAsDataURL(file);
+            }
+            setFileToUpload({ ...fileToUpload, file: e.target.files[0] });
+          }}
         />
       </div>
       <button
